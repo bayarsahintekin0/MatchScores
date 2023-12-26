e7386870ab8adcd7bd9309dd5155e91019214f2c
bayar.sahintekin <bayar.sahintekin@sisal.com>
2023-12-26 12:45:10 +0300

    CI: Qodana test

--- presentation/src/main/java/com/bayarsahintekin/matchscores/MainActivity.kt
+++ presentation/src/main/java/com/bayarsahintekin/matchscores/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = Color.White) {
                     MainScreenView()
-                } \??
+                }
             }
         }
 
